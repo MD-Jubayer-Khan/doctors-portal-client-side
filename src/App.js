eb759934add0689710871777ca23a635f7fab04c
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-06 16:51:57 +0600

    send booking data to database to handle multiple booking and setup react query

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './Pages/Login/Login';
 import RequireAuth from './Pages/Login/RequireAuth';
 import Navbar from './Pages/Shared/Navbar';
 import SignUp from './Pages/Login/SignUp';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
           }></Route>
 
         </Routes>
+        <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,17 +2,50 @@ import { format } from 'date-fns';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
+import { success } from 'daisyui/src/colors';
 
 
 const BookingModal = ({date, treatment, setTreatment}) => {
-    const {name, slots} = treatment;
+    const {_id, name, slots} = treatment;
     const [user] = useAuthState(auth);
+    const formattedDate = format(date, 'PP')
 
     const handleSubmit = event =>{
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(slot, name);
-        setTreatment(null) 
+
+        const booking = {
+          treatmentId : _id,
+          treatment : name,
+          slot,
+          date: formattedDate,
+          patient: user.email,
+          patientName: user.displayName,
+          phone: event.target.phone.value
+
+        }
+
+        fetch('http://localhost:5000/booking', {
+          method: 'post',
+          headers:{
+            'content-type' : 'application/json'
+          },
+          body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if(data.success){
+            toast(`Appointment is set on, ${formattedDate} at ${slot}`)
+          }
+          else{
+            toast.error(`Already have an appointment on, ${formattedDate} at ${slot}`)
+          }
+
+        })
+
+        setTreatment(null);
 
     };
 
